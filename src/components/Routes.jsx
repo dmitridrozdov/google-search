35b1ec2acba630fdac64419c2ee3fee36e3959a2
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-03 16:33:29 +1100

    Update Routes.jsx

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Routes = () => {
+export const Routes = () => {
     return (
         <div>
             <h1>Routes</h1>
         </div>
     )
 }
-
-export default Routes
