22e7189095a3064d9f609cb609b744b3279223f5
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-09 19:21:37 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     return (
         <div className={darkTheme ? 'dark' : ''}>
             <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
-                <Navbar />
+                <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme}/>
                 <Routes /> 
                 <Footer />
             </div>
