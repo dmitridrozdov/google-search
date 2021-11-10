aa8237c7426f9cbb4ca12026ec0bc3217248eaf6
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-10 11:21:50 +1100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
+            Styles
         </div>
     )
 }
