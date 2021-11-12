c1f8a7f8627835facc15f432db977063b88cf819
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-12 11:50:54 +1100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Navbar = () => {
+export const Navbar = ({ setDarkTheme, darkTheme }) => {
     return (
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
             <div className="flex justify-between items-center space-x-5 w-screen ">
