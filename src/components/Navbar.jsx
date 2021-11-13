1a6cfb2901d8d395f6c5190800e237f0142a01d4
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-13 19:09:10 +1100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { Search } from './Search'
+
 export const Navbar = ({ setDarkTheme, darkTheme }) => {
     return (
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
@@ -9,15 +11,13 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => {
                     <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
                         Search 🔎
                     </p> 
-                    
                 </Link>
                 <button type="button" onClick={() => setDarkTheme(!darkTheme)} 
                     className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
                     {darkTheme ? '💡 Light' : '🌙 Dark'}
                 </button>
-                
             </div>
-            
+            <Search />
         </div>
     )
 }
